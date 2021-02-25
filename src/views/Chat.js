5901096427a1d8381d5bd49ae4501c863662380f
Zulfikar Fajri <fajrizulfikar85@gmail.com>
2021-02-25 23:52:58 +0700

    feat: create footer component

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -2,14 +2,78 @@ import React from 'react';
 import {
   View,
   Text,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import getFontRatio from '../libs/screen/getFontRatio';
+import getOrientation from '../libs/screen/getOrientation';
 
 const Chat = () => {
   return (
-    <View>
-      <Text>Chat</Text>
+    <View style={styles.body}>
+      {/* <FlatList
+        inverted={true}
+        data={['belek', 'bulek', 'balek']}
+        renderItem={({ item, index }) => (
+            <View key={`chat_item_${index.toString()}`}>
+              <Text>
+                {item}
+              </Text>
+            </View>
+          )
+        }
+      /> */}
+      <View 
+        style={[
+          styles.footerContainer,
+          { maxHeight: getOrientation() == 'landscape' ? '10%' : '5%' }
+        ]}
+      >
+        <View style={styles.footerTextWrapper}>
+          <TextInput
+            multiline
+            numberOfLines={1}
+            style={{ textAlignVertical: 'top' }}
+          />
+        </View>
+        <TouchableOpacity style={styles.footerButtonContainer}>
+          <Icon
+            name='send'
+            size={getFontRatio(25)}
+            color='#fff'
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  footerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  footerTextWrapper: {
+    width: '90%',
+    borderWidth: 1.5,
+    borderColor: 'grey',
+  },
+  footerButtonContainer: {
+    width: '10%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1.5,
+    borderColor: 'grey',
+    backgroundColor: 'rgb(55, 222, 85)',
+  },
+});
 
 export default Chat;
